6e7c517079907ff637d07ac71c5c9b495fc98ca8
KrasnovYuri <krasnov1yuri@gmail.com>
2023-12-03 19:19:39 +0300

    add WelcomeVC

---
+++ BookStore/Controllers/WelcomeViewController/WelcomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeViewController.swift
+//  BookStore
+//
+//  Created by Юрий on 03.12.2023.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}

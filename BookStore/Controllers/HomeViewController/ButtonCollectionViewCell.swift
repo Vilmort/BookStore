917b562fa1e7c046e36ab9fc692ce748971d101b
udachi tomo <fragnet1ck@gmail.com>
2023-12-11 23:11:17 +0300

    1. реализован основной UI 2. начата работа с данными из сети

---
+++ BookStore/Controllers/HomeViewController/ButtonCollectionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  ButtonCollectionViewCell.swift
+//  BookStore
+//
+//  Created by User on 11.12.2023.
+//
+
+import Foundation
---
+++ BookStore/Controllers/HomeViewController/HomeCollectionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  HomeCollectionViewCell.swift
+//  BookStore
+//
+//  Created by User on 09.12.2023.
+//
+
+import Foundation

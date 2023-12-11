afe3f85d18ea5535abe86c79b482887d0ee4ba2f
KrasnovYuri <krasnov1yuri@gmail.com>
2023-12-11 17:45:30 +0300

    delete comments in WelcomeVC and start create BookDescriptionVC

---
+++ BookStore/Controllers/BookDescription/BookDescriptionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BookDeskriptionViewController.swift
+//  BookStore
+//
+//  Created by Юрий on 11.12.2023.
+//
+
+import UIKit
+
+class BookDescriptionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
